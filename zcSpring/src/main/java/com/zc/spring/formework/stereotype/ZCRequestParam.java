47d53e160bdb7322ef470a602d0c3dd3b8f540af
zhangc <358333128@qq.com>
2019-04-17 09:10:45 +0800

    Spring mvc

--- zcSpring/src/main/java/com/zc/spring/formework/stereotype/ZCRequestParam.java
+++ zcSpring/src/main/java/com/zc/spring/formework/stereotype/ZCRequestParam.java
@@ -2,11 +2,6 @@ package com.zc.spring.formework.stereotype;
 
 import java.lang.annotation.*;
 
-/**
- * 请求参数映射
- * @author Tom
- *
- */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
